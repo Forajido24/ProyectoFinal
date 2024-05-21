6377fe7dcd0618935961bbfb8650a52efe925d2f
Forajido24 <159275784+Forajido24@users.noreply.github.com>
2024-05-21 15:39:52 -0600

    2da actualización con warahouse
    colocamos las distancias entre nodos y el warehouse

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from tkinter import filedialog, Toplevel, Canvas, Frame, Label, Text, Button, Ph
 import math
 from tkinter import *
 
-
 def get_location(df):
     return df["Location"].tolist()
 
@@ -126,7 +125,7 @@ def solve_vrp():
     display_graph(used_nodes)
 
 def display_graph(used_nodes):
-    global location, notation, selected_connections
+    global location, notation, selected_connections, dis_w, dis
 
     graph_window = Toplevel(root)
     graph_window.title("VRP Solution Graph")
@@ -150,16 +149,25 @@ def display_graph(used_nodes):
         canvas.create_oval(x - 10, y - 10, x + 10, y + 10, fill="blue",width=3)
         canvas.create_text(x, y - 15, text=notation[i], fill="black",width=3)
 
-    for i, j, _ in selected_connections:
+    # Dibujar el nodo central (Warehouse)
+    canvas.create_oval(center_x - 10, center_y - 10, center_x + 10, center_y + 10, fill="green",width=3)
+    canvas.create_text(center_x, center_y - 15, text="Warehouse", fill="black")
+
+    # Conectar Warehouse con todos los nodos y mostrar distancias
+
+    for i, j, distance in selected_connections:
         x1, y1 = node_positions[i]
         x2, y2 = node_positions[j]
-        canvas.create_line(x1, y1, x2, y2, fill="red", width=3)
+        canvas.create_line(x1, y1, x2, y2, fill="red",width=3)
+        mid_x = (x1 + x2) // 2
+        mid_y = (y1 + y2) // 2
+        canvas.create_text(mid_x, mid_y, text=f"{distance} km", fill="black")
 
     for i in range(len(location)):
         if used_nodes[i] == 0:
             x, y = node_positions[i]
-            canvas.create_oval(x - 10, y - 10, x + 10, y + 10, fill="blue",width=3)
-            canvas.create_text(x, y - 15, text=notation[i], fill="black",width=3)
+            canvas.create_oval(x - 10, y - 10, x + 10, y + 10, fill="blue",width=2)
+            canvas.create_text(x, y - 15, text=notation[i], fill="black",width=2)
 
 root = Tk()
 root.title("Problema del Enrutamiento del Vehículo")
@@ -183,6 +191,6 @@ Texto_data.place(x=25, y=165, width=150, height=20)
 nombre_data = Text(frm)
 nombre_data.place(x=160, y=165, width=200, height=20)
 
-boton = Button(frm, text="Seleccionar archivo", command=select_file, font=("Arial", 10, "bold"), fg="white", activeforeground="black", bg="#43444A", activebackground="lightblue", relief="raised")
+boton = Button(frm, text="Selccionar archivo", command=select_file, font=("Arial", 10, "bold"), fg="white", activeforeground="black", bg="#43444A", activebackground="lightblue", relief="raised")
 boton.place(x=185, y=195)
 root.mainloop()
